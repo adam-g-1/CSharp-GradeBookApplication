41f3ecb05e995d873295495a09bf9994b1ecc39a
adam-g-1 <60746720+adam-g-1@users.noreply.github.com>
2020-04-06 12:53:30 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,13 +15,13 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-            if (averageGrade >= 90)
+            if (averageGrade >= 80)
                 return 'A';
-            else if (averageGrade >= 80)
-                return 'B';
-            else if (averageGrade >= 70)
-                return 'C';
             else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
                 return 'D';
             else
                 return 'F';
